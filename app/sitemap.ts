95e6c220408d6b2fa49c931d1df79ac32171d47f
Chheng Bunheang <2005chhengbunheang@gmail.com>
2026-03-12 12:59:52 +0700

    add sitemap and rebots

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://chhengbunheang.dev",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://chhengbunheang.dev/projects",
+      lastModified: new Date(),
+    },
+  ];
+}
